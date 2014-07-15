561ca4e08338a730e9c9badc36d6144dbaa9934a
Xiang <xiang@silvustechnologies.com>
2014-07-14 18:55:26 -0700

    off_tutor part1

--- off_tutor/mysite/polls/models.py
+++ off_tutor/mysite/polls/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+#each model is represented by a class
+#each model has a number of class variables, each of which represents a database field in the model
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    def __unicode__(self):
+        return self.question
+
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
-    choice text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __unicode__(self):
+        return self.choice_text
