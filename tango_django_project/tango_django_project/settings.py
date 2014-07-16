2b6e2efb65b49d3d15a8e0bccd0be8cd9ae903b0
Xiang <xiang@silvustechnologies.com>
2014-07-15 18:42:05 -0700

    add static media try development static media server

--- tango_django_project/tango_django_project/settings.py
+++ tango_django_project/tango_django_project/settings.py
@@ -4,6 +4,8 @@ import os
 SETTINGS_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_PATH = os.path.dirname(SETTINGS_DIR)
 TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+#static
+STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -55,12 +57,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -77,6 +79,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_PATH,
 )
 
 # List of finder classes that know how to find static files in
