266fe8fd0c53f74cc2f0dd6dda00f58c9b0cae49
Xiang <xiang@silvustechnologies.com>
2014-07-17 17:53:15 -0700

    part 1

--- off_tutor/mysite/polls/models.py
+++ off_tutor/mysite/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,6 +11,8 @@ class Poll(models.Model):
     pub_date = models.DateTimeField("date published")
     def __unicode__(self):
         return self.question
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
