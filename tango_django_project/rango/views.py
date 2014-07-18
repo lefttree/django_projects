d9d054eee45bab04b002243fa5df40ff64f63754
Xiang <xiang@silvustechnologies.com>
2014-07-18 16:46:51 -0700

    category page

--- tango_django_project/rango/views.py
+++ tango_django_project/rango/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rango.forms import CategoryForm
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
     #return HttpResponse("Rango says hello world! <a href='/rango/about'>About</a>")
@@ -11,6 +11,10 @@ def index(request):
     #query the database
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
+
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
+
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
@@ -42,4 +46,22 @@ def add_category(request):
         form = CategoryForm()
 
     return render_to_response('rango/add_category.html', {'form': form}, context)
+
+def category(request, category_name_url):
+   context = RequestContext(request)
+   category_name = category_name_url.replace('_', ' ')
+
+   context_dict = {'category_name': category_name}
+   try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+   except Category.DoesNotExist:
+        pass
+
+   return render_to_response('rango/category.html', context_dict, context)
+
+
 
