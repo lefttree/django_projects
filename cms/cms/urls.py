8f5513d67996219b4462a4e49bd77d3b2c623adc
Xiang <xiang@silvustechnologies.com>
2014-12-08 19:03:13 -0800

    add tinymce the change_form.html template is valid for django 1.6+ please find the correct for 1.5.4

--- cms/cms/urls.py
+++ cms/cms/urls.py
@@ -1,4 +1,9 @@
 from django.conf.urls import patterns, include, url
+import os
+SETTINGS_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_PATH = os.path.dirname(SETTINGS_DIR)
+TINYMCE_PATH = PROJECT_PATH + "/tinymce/js/tinymce"
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,5 +20,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    #url(r'^tiny_mce/(?P<path>.*)$', 'django.views.static.server',{ 'document_root': TINYMCE_PATH}),
     url(r'', include('django.contrib.flatpages.urls')),
 )
